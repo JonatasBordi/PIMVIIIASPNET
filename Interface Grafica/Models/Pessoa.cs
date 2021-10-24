d4345fae224fb73e8dd834671b7ca1a210fa60ca
edugotinha <eduardo.l.souza@outlook.com>
2021-10-24 14:50:45 -0300

    primeiro commid

---
+++ Interface Grafica/Models/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Interface_Grafica.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+
+    }
+}
